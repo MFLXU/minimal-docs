dd944da580877995e8f25b240a7356ff7db48165
Lou+^*! <90705868+MFLXU@users.noreply.github.com>
2022-04-08 19:13:34 +0200

    responsive update

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ let navBar = document.querySelector(".navbar")
 navBtn.addEventListener("click", () => {
     navBar.classList.toggle("show-nav")
     if(navBtn.className ===  "fa-solid fa-bars"){
-        navBtn.classList === "fa-solid fa-xmark"
+        navBtn.className = "fa-solid fa-xmark"
     }
     else{
-        navBtn.classList === 'fa-solid fa-bars'
+        navBtn.className = 'fa-solid fa-bars'
     }
 })
